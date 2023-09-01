e055d1612ee0ee18ef1155737e94fb2b1909fb89
Mostafa Safwat <mostafasafwat404@gmail.com>
2023-09-01 19:51:52 +0300

    converted function to api

--- Amazon price webscraper.py
+++ Amazon price webscraper.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, jsonify
 import requests
 import time
 import os
@@ -13,36 +14,34 @@ client = MongoClient(mongo_uri)
 db = client["mydatabase"]
 collection = db["prices"]
 
-url = input('Enter the URL of the product you want to track: ').replace("amazon.eg", "amazon.eg/-/en")
-def webscrape():
-    try:
-        headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0'}
-        page = requests.get(url, headers=headers)
-        print(page.status_code)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        title = soup.find(id="productTitle").get_text().strip()
-        price = soup.find(class_="a-price-whole").get_text().replace(",", "").replace(".", "")
-        currency = soup.find(class_="a-price-symbol").get_text()
-        price = float(price)
+app = Flask(__name__)
 
-        asin = ""
-        for path in url.split("/"):
-            if path.startswith("B0"):
-                asin = path.split("?")[0]
+@app.route('/price', methods=['POST'])
+def get_price():
+    url = request.json['url']
+    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0'}
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find(id="productTitle").get_text().strip()
+    price = soup.find(class_="a-price-whole").get_text().replace(",", "").replace(".", "")
+    currency = soup.find(class_="a-price-symbol").get_text()
+    price = float(price)
 
-        old_product = collection.find_one({"asin": asin})
-        if(not old_product):
-            collection.insert_one({"title": title, "price": price, "currency": currency, "asin": asin})
-        else:
-            collection.update_one({"asin": asin}, {"$set": {"price": price, "currency": currency}})
-        old_price = old_product["price"]
+    asin = ""
+    for path in url.split("/"):
+        if path.startswith("B0"):
+            asin = path.split("?")[0]
+
+    old_product = collection.find_one({"asin": asin})
+    if(not old_product):
+        collection.insert_one({"title": title, "price": price, "currency": currency, "asin": asin})
+    else:
+        old_price = old_product['price']
         if price < old_price:
-            print(f"Price of {title} has changed to: {currency} {str(price)}")
+            collection.update_one({"asin": asin}, {"$set": {"price": price}})
+            return jsonify({"message": f"Price of {title} has decreased from {old_price} to {currency} {price}"})
         else:
-            print(f"Price of {title} has not changed: {str(price)}")
-    except:
-        print("Error: Could not retrieve price. Retrying in 5 seconds...")
-        time.sleep(5)
-        webscrape()
-webscrape()
+            return jsonify({"message": f"Price of {title} has not changed: {currency} {price}"})
 
+if __name__ == '__main__':
+    app.run(debug=True)
